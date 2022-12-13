61a34f764114fe59257641d967fbdd3866f7ec63
Tran Tan Dat <dattrantan01@gmail.com>
2022-12-13 19:29:28 +0700

    nffakn

--- src/components/dashboard-sidebar.js
+++ src/components/dashboard-sidebar.js
@@ -1,61 +1,51 @@
-import { useEffect } from 'react';
-import NextLink from 'next/link';
-import { useRouter } from 'next/router';
-import PropTypes from 'prop-types';
-import { Box, Button, Divider, Drawer, Typography, useMediaQuery } from '@mui/material';
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
-import { ChartBar as ChartBarIcon } from '../icons/chart-bar';
-import { Cog as CogIcon } from '../icons/cog';
-import { Lock as LockIcon } from '../icons/lock';
-import { Selector as SelectorIcon } from '../icons/selector';
-import { User as UserIcon } from '../icons/user';
-import { UserAdd as UserAddIcon } from '../icons/user-add';
-import { Users as UsersIcon } from '../icons/users';
-import { XCircle as XCircleIcon } from '../icons/x-circle';
-import BedroomParentIcon from '@mui/icons-material/BedroomParent';
-import MapsHomeWorkIcon from '@mui/icons-material/MapsHomeWork';
-import { Logo } from './logo';
-import { NavItem } from './nav-item';
+import { useEffect } from "react";
+import NextLink from "next/link";
+import { useRouter } from "next/router";
+import PropTypes from "prop-types";
+import { Box, Button, Divider, Drawer, Typography, useMediaQuery } from "@mui/material";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import { ChartBar as ChartBarIcon } from "../icons/chart-bar";
+import { Cog as CogIcon } from "../icons/cog";
+import { Lock as LockIcon } from "../icons/lock";
+import { Selector as SelectorIcon } from "../icons/selector";
+import { User as UserIcon } from "../icons/user";
+import { UserAdd as UserAddIcon } from "../icons/user-add";
+import { Users as UsersIcon } from "../icons/users";
+import { XCircle as XCircleIcon } from "../icons/x-circle";
+import BedroomParentIcon from "@mui/icons-material/BedroomParent";
+import MapsHomeWorkIcon from "@mui/icons-material/MapsHomeWork";
+import { Logo } from "./logo";
+import { NavItem } from "./nav-item";
 
 const items = [
   {
-    href: '/',
-    icon: (<ChartBarIcon fontSize="small" />),
-    title: 'Dashboard'
+    href: "/",
+    icon: <ChartBarIcon fontSize="small" />,
+    title: "Dashboard",
   },
   {
-    href: '/customers',
-    icon: (<UsersIcon fontSize="small" />),
-    title: 'Customers'
+    href: "/customers",
+    icon: <UsersIcon fontSize="small" />,
+    title: "Customers",
   },
   {
-    href: '/properties',
-    icon: (<MapsHomeWorkIcon fontSize="small" />),
-    title: 'Properties'
+    href: "/properties",
+    icon: <MapsHomeWorkIcon fontSize="small" />,
+    title: "Properties",
   },
   {
-    href: '/rooms',
-    icon: (<BedroomParentIcon fontSize="small" />),
-    title: 'Rooms'
+    href: "/rooms",
+    icon: <BedroomParentIcon fontSize="small" />,
+    title: "Rooms",
   },
-  {
-    href: '/settings',
-    icon: (<CogIcon fontSize="small" />),
-    title: 'Settings'
-  },
-  {
-    href: '/404',
-    icon: (<XCircleIcon fontSize="small" />),
-    title: 'Error'
-  }
 ];
 
 export const DashboardSidebar = (props) => {
   const { open, onClose } = props;
   const router = useRouter();
-  const lgUp = useMediaQuery((theme) => theme.breakpoints.up('lg'), {
+  const lgUp = useMediaQuery((theme) => theme.breakpoints.up("lg"), {
     defaultMatches: true,
-    noSsr: false
+    noSsr: false,
   });
 
   useEffect(
@@ -76,55 +66,44 @@ export const DashboardSidebar = (props) => {
     <>
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          height: '100%'
+          display: "flex",
+          flexDirection: "column",
+          height: "100%",
         }}
       >
         <div>
           <Box sx={{ p: 3 }}>
-            <NextLink
-              href="/"
-              passHref
-            >
+            <NextLink href="/" passHref>
               <a>
                 <Logo
                   sx={{
                     height: 42,
-                    width: 42
+                    width: 42,
                   }}
                 />
               </a>
             </NextLink>
           </Box>
-          <Box sx={{ px: 2 }}>
-            
-          </Box>
+          <Box sx={{ px: 2 }}></Box>
         </div>
         <Divider
           sx={{
-            borderColor: '#2D3748',
-            my: 3
+            borderColor: "#2D3748",
+            my: 3,
           }}
         />
         <Box sx={{ flexGrow: 1 }}>
           {items.map((item) => (
-            <NavItem
-              key={item.title}
-              icon={item.icon}
-              href={item.href}
-              title={item.title}
-            />
+            <NavItem key={item.title} icon={item.icon} href={item.href} title={item.title} />
           ))}
         </Box>
-        <Divider sx={{ borderColor: '#2D3748' }} />
+        <Divider sx={{ borderColor: "#2D3748" }} />
         <Box
           sx={{
             px: 2,
-            py: 3
+            py: 3,
           }}
-        >
-        </Box>
+        ></Box>
       </Box>
     </>
   );
@@ -136,10 +115,10 @@ export const DashboardSidebar = (props) => {
         open
         PaperProps={{
           sx: {
-            backgroundColor: 'neutral.900',
-            color: '#FFFFFF',
-            width: 280
-          }
+            backgroundColor: "neutral.900",
+            color: "#FFFFFF",
+            width: 280,
+          },
         }}
         variant="permanent"
       >
@@ -155,10 +134,10 @@ export const DashboardSidebar = (props) => {
       open={open}
       PaperProps={{
         sx: {
-          backgroundColor: 'neutral.900',
-          color: '#FFFFFF',
-          width: 280
-        }
+          backgroundColor: "neutral.900",
+          color: "#FFFFFF",
+          width: 280,
+        },
       }}
       sx={{ zIndex: (theme) => theme.zIndex.appBar + 100 }}
       variant="temporary"
@@ -170,5 +149,5 @@ export const DashboardSidebar = (props) => {
 
 DashboardSidebar.propTypes = {
   onClose: PropTypes.func,
-  open: PropTypes.bool
+  open: PropTypes.bool,
 };
